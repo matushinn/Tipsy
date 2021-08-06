3c0edb4d1895f917a3fa60bbd1794f9e040eb665
shotaro-ooe <shoooe97@gmail.com>
2021-08-06 10:10:04 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#  Tipsy
+
+## Our Goal
+The goal of this challenge is to review some of the concepts learnt in the previous modules.Especially relating to the creation of multi-screen apps through the usee of seague.
+
+## What you will create
+By the end of the module,you will have made beautiful bill splitting , tip calculating app,No one can work out $145,56 ssplit between 5 people with a 20 percent tip.With your very own Tipssy app in hand, you'll never need to do maths ever again!
+
+
---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by 大江祥太郎 on 2021/08/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    @IBAction func stepperValueChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func calculatorPressed(_ sender: UIButton) {
+    }
+    
+}
+
